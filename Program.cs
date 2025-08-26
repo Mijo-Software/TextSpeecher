b8ffecaac6b0f683b53a336fae815d632489615a
Michael de Radobýl <mjohne@users.noreply.github.com>
2025-08-26 11:47:21 +0200

    fixed some errors and improved code handling

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace TextSpeecher
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+            Application.Run(mainForm: new MainForm());
         }
     }
 }
